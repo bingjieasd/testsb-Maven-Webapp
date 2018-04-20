85e7fcbd7540504b9b76faf6dd6790c22eb884c9
bingjieasd <912295735@qq.com>
2018-04-20 15:08:50 +0800

    2.0
    rrrr

--- src/main/java/app/RunStart.java
+++ src/main/java/app/RunStart.java
@@ -14,6 +14,7 @@ public class RunStart extends SpringBootServletInitializer{
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(RunStart.class);
 	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(RunStart.class, args);
